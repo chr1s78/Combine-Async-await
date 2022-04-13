5e03ac3fb9d893f91547eccb165ffc6e1e7bd3bf
chr1s78 <ayahirano2014@126.com>
2022-04-13 13:36:22 +0800

    Initial Commit

---
+++ CombineAsync/CombineAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAsyncApp.swift
+//  CombineAsync
+//
+//  Created by Chr1s on 2022/4/13.
+//
+
+import SwiftUI
+
+@main
+struct CombineAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineAsync/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineAsync
+//
+//  Created by Chr1s on 2022/4/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
